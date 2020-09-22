cf2a7d522dd78834da1dc0cc24c95fa45c4173d0
Jaakko Kantojärvi <jaakko@n-1.fi>
2020-09-22 08:07:14 +0300

    Add hack: hide menu bar in draw.io

---
+++ drawio.modify.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+root=$1
+resources="$root/opt/draw.io/resources"
+sources="$PWD/drawio"
+
+[[ $root && -d $root ]] || { echo "Missing '$root'" >&2; exit 1; }
+
+asar="$PWD/node_modules/.bin/asar"
+[[ -f $asar ]] || npm install --no-save --engine-strict asar
+
+set -x
+
+# extract asar
+app="$root/app"
+[[ -e $app ]] && rm -rf "$app"
+[[ -e "$resources/app.asar" ]] || { echo "Missing '$resources/app.asar'" >&2; exit 1; }
+"$asar" extract "$resources/app.asar" "$app"
+
+# Copy files
+cp "$sources/disableUpdate.js" "$app/"
+
+# Patch indexjs
+sed -i \
+	-e 's,^\(\s*const menuBar = menu\.buildFromTemplate(template)\)$,/* XXX: remove menu */\n//\1,' \
+	-e 's,^\(\s*menu\.setApplicationMenu(menuBar)\)$,//\1,' \
+	-e '/^    let win = createWindow()/a\    /* XXX: remove menu */\n    win.removeMenu()' \
+	"$app/electron.js"
+grep -qsE '^\s*//.*setApplicationMenu' "$app/electron.js" || { echo "Fixing menu failed! (1)" >&2; exit 1; }
+grep -qsE '^\s*win\.removeMenu' "$app/electron.js" || { echo "Fixing menu failed! (2)" >&2; exit 1; }
+
+# Repackage
+rm -f "$resources/app.asar"
+"$asar" pack "$app" "$resources/app.asar"
+
+# cleanup
+rm -rf "$app"
---
+++ drawio/disableUpdate.js
@@ -0,0 +1,5 @@
+module.exports = {
+	disableUpdate: function() {
+		return true;
+	}
+}
